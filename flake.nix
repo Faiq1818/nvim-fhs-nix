cd95e692e8305951be0ab6ffbd20eaccf9fc2f30
faiq1818 <ghozyerlanggafaiq@gmail.com>
2026-06-13 01:43:05 +0700

    feat(flake): add flake.nix and flake.lock

---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+      neovim-fhs = pkgs.buildFHSEnv {
+        name = "nvim";
+
+        targetPkgs =
+          pkgs: with pkgs; [
+            neovim
+            git
+            curl
+            gnumake
+            gcc
+            unzip
+            gnutar
+            gzip
+            ripgrep
+            fd
+
+            glibc
+            stdenv.cc.cc.lib
+          ];
+
+        multiPkgs =
+          pkgs: with pkgs; [
+            zlib
+          ];
+
+        runScript = pkgs.writeShellScript "nvim-wrapper" ''
+          unset VIMINIT
+          unset VIM
+          unset MYVIMRC
+
+          export PATH="$PATH:/run/current-system/sw/bin:/etc/profiles/per-user/$USER/bin"
+
+          exec nvim "$@"
+        '';
+      };
+    in
+    {
+      packages.${system}.default = neovim-fhs;
+
+      apps.${system}.default = {
+        type = "app";
+        program = "${neovim-fhs}/bin/nvim";
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ neovim-fhs ];
+      };
+    };
+}
